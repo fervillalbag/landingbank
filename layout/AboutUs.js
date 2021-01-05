87ae286bb15343ad18ec36abcd5b6c8c078df131
fervillalbag <21fervillalbag@gmail.com>
2021-01-05 02:22:26 -0300

    Changing a button to link

--- layout/AboutUs.js
+++ layout/AboutUs.js
@@ -1,11 +1,15 @@
 
-import React from 'react'
+import Link from 'next/link'
 
 export default function AboutUs() {
    return (
       <section className="about container">
          <h2 className="about__title">Find out more about how we work</h2>
-         <button className="about__button">How we work</button>
+         <Link href="/">
+            <a className="about__button">
+               How we work
+            </a>
+         </Link>
       </section>
    )
 }
